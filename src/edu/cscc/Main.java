fa88b2792346c90ce5c09c30718d6044c699c2f6
aerolona <aerolona@hotmail.com>
2019-06-05 01:27:01 -0400

    added code to output "Hello, World!"

--- src/edu/cscc/Main.java
+++ src/edu/cscc/Main.java
@@ -1,8 +1,10 @@
+//Sherin Elbokhary. 6/5/2019. Simple Java application to output "Hello, World!" and pushhing it to GitHub
 package edu.cscc;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.print("Hello, World!");
     }
 }
